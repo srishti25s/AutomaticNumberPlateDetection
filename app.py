b57f6b8e7fecb4dc4dc16431cd1f7a3fd57eec13
Yash Kanojia <37548248+yashkan27@users.noreply.github.com>
2020-10-17 15:31:35 +0530

    Handled the plate_result folder issue.

--- app.py
+++ app.py
@@ -1,29 +1,36 @@
-from flask import Flask, render_template, request, redirect,url_for
+from flask import Flask, render_template, request, redirect,url_for,Response
 from testing import start_video
-from os import listdir
-from os.path import isfile, join
+import os
 
+import shutil
+import cv2
 
 app = Flask(__name__)
-//rtsp://100.91.39.237:5554
 
 VIDEO_UPLOAD_FILE_PATH = "static/videos"
 IMAGE_PATH = "static/plate_result"
 
+if os.path.exists(IMAGE_PATH):
+	shutil.rmtree(IMAGE_PATH)
+
+os.mkdir(IMAGE_PATH)
+
 @app.route("/")
 def home():
-        files_list = [f for f in listdir(VIDEO_UPLOAD_FILE_PATH) if isfile(join(VIDEO_UPLOAD_FILE_PATH, f))]
-        return render_template("home.html",files=files_list)
+    files_list = [f for f in os.listdir(VIDEO_UPLOAD_FILE_PATH) if os.path.isfile(os.path.join(VIDEO_UPLOAD_FILE_PATH, f))]
+    return render_template("home.html",files=files_list)
 
 @app.route("/renderVideo",methods=["POST"])
 def render_video():
-    start_video(str(request.data))
+    path = request.data.decode("utf-8") 
+    print('Path -',path)
+    start_video(path)
     return "Success"
-    
+
 @app.route("/numberPlatesList",methods=["POST"])
 def render_number_plate():
-    files_list = [f for f in listdir(IMAGE_PATH) if isfile(join(IMAGE_PATH, f))]
+    files_list = [f for f in os.listdir(IMAGE_PATH) if os.path.isfile(os.path.join(IMAGE_PATH, f))]
     return ','.join(files_list)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1",port=5000,debug=True)
